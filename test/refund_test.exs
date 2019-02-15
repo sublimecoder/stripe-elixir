942550e49eb8b2cf51c0d9abc1f4f990813cbeb6
Jared Smith <jared.smith88@me.com>
2019-02-14 16:35:30 -0800

    Fix test failure due to hard coded dates

--- test/refund_test.exs
+++ test/refund_test.exs
@@ -3,30 +3,26 @@ defmodule Stripe.RefundTest do
 
   alias Stripe.{Token, Charge, Refund}
   alias Stripe.InvalidRequestError
+  alias Stripe.Fixture.Token, as: TokenFixture
 
   test "create/update a refund" do
-    {:ok, token} = Token.create(
-      card: [
-        number: "4242424242424242",
-        exp_month: 7,
-        exp_year: 2018,
-        cvc: "314"
-      ]
-    )
+    {:ok, token} = TokenFixture.valid_card() |> Token.create()
 
     {:ok, charge} = Charge.create(amount: 1000, source: token["id"], currency: "usd")
 
     assert {:ok, %{"amount" => 1000} = refund} = Refund.create(charge: charge["id"])
-    assert {:ok, %{"metadata" => %{"key" => "value"}}} = Refund.update(refund["id"], metadata: [key: "value"])
+
+    assert {:ok, %{"metadata" => %{"key" => "value"}}} =
+             Refund.update(refund["id"], metadata: [key: "value"])
   end
 
   test "retrieve a refund" do
-    assert {:error, %InvalidRequestError{message: "No such refund: not exist"}}
-      = Refund.retrieve("not exist")
+    assert {:error, %InvalidRequestError{message: "No such refund: not exist"}} =
+             Refund.retrieve("not exist")
   end
 
   test "list all refunds" do
-    assert {:ok, %{"object" => "list", "url" => "/v1/refunds"}}
-      = Refund.list
+    assert {:ok, %{"object" => "list", "url" => "/v1/refunds"}} =
+             Refund.list()
   end
 end
