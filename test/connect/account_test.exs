15e6e90d050995e0f39db0d09635e882e58555b0
Jared Smith <jared.smith88@me.com>
2019-02-14 16:20:41 -0800

    Fix hard coded expiration dates causing test failures

--- test/connect/account_test.exs
+++ test/connect/account_test.exs
@@ -1,61 +1,55 @@
 defmodule Stripe.AccountTest do
   use ExUnit.Case
+  alias Stripe.Fixture.Token, as: TokenFixture
 
   test "create/update/retrieve/delete an account" do
     assert {:ok, account} = Stripe.Account.create(type: "custom")
     assert {:ok, %{"email" => _email}} = Stripe.Account.retrieve(account["id"])
+
     assert {:ok, %{"metadata" => %{"test" => "data"}}} =
-      Stripe.Account.update(account["id"], metadata: [test: "data"])
+             Stripe.Account.update(account["id"], metadata: [test: "data"])
+
     assert {:ok, _} = Stripe.Account.delete(account["id"])
     assert {:error, _} = Stripe.Account.retrieve(account["id"])
   end
 
   test "list all accounts" do
-    assert {:ok, %{"object" => "list", "url" => "/v1/accounts"}} = Stripe.Account.list
+    assert {:ok, %{"object" => "list", "url" => "/v1/accounts"}} = Stripe.Account.list()
   end
 
   test "create/update/retrieve/delete/list an external_account" do
     {:ok, account} = Stripe.Account.create(type: "custom")
 
-    {:ok, token} = Stripe.Token.create(
-      card: [
-        number: "4000056655665556",
-        exp_month: 7,
-        exp_year: 2018,
-        cvc: "314",
-        currency: "usd"
-      ]
-    )
-
-    {:ok, token2} = Stripe.Token.create(
-      card: [
-        number: "5200828282828210",
-        exp_month: 7,
-        exp_year: 2018,
-        cvc: "314",
-        currency: "usd"
-      ]
-    )
+    {:ok, token} = TokenFixture.visa_debit_card() |> Stripe.Token.create()
+    {:ok, token2} = TokenFixture.master_card_debit_card() |> Stripe.Token.create()
 
     account_id = account["id"]
 
     assert {:ok, external_account} =
-      Stripe.Account.create_external_account(account_id, external_account: token["id"])
+             Stripe.Account.create_external_account(account_id, external_account: token["id"])
+
     assert {:ok, external_account2} =
-      Stripe.Account.create_external_account(account_id, external_account: token2["id"])
+             Stripe.Account.create_external_account(account_id, external_account: token2["id"])
 
     url = "/v1/accounts/#{account_id}/external_accounts"
 
-    assert {:ok, %{"object" => "list", "url" => ^url}}
-      = Stripe.Account.list_external_account(account_id)
+    assert {:ok, %{"object" => "list", "url" => ^url}} =
+             Stripe.Account.list_external_account(account_id)
 
     external_account_id = external_account["id"]
     external_account_id2 = external_account2["id"]
 
-    assert {:ok, ^external_account} = Stripe.Account.retrieve_external_account(account_id, external_account_id)
+    assert {:ok, ^external_account} =
+             Stripe.Account.retrieve_external_account(account_id, external_account_id)
+
     assert {:ok, %{"metadata" => %{"test" => "data"}}} =
-      Stripe.Account.update_external_account(account_id, external_account_id, metadata: [test: "data"])
+             Stripe.Account.update_external_account(account_id, external_account_id,
+               metadata: [test: "data"]
+             )
+
     assert {:ok, _} = Stripe.Account.delete_external_account(account_id, external_account_id2)
-    assert {:error, _} = Stripe.Account.retrieve_external_account(account_id, external_account_id2)
+
+    assert {:error, _} =
+             Stripe.Account.retrieve_external_account(account_id, external_account_id2)
   end
 end
--- test/support/token_fixture.ex
+++ test/support/token_fixture.ex
@@ -1,21 +1,48 @@
 defmodule Stripe.Fixture.Token do
-
   def valid_card() do
-    %{card: [
-      number: "4242424242424242",
-      exp_month: exp_month(),
-      exp_year: exp_year(),
-      cvc: "314"
-    ]}
+    %{
+      card: [
+        number: "4242424242424242",
+        exp_month: exp_month(),
+        exp_year: exp_year(),
+        cvc: "314"
+      ]
+    }
   end
 
   def invalid_card() do
-    %{card: [
-      number: "invalid card number",
-      exp_month: exp_month(),
-      exp_year: exp_year(),
-      cvc: "314"
-    ]}
+    %{
+      card: [
+        number: "invalid card number",
+        exp_month: exp_month(),
+        exp_year: exp_year(),
+        cvc: "314"
+      ]
+    }
+  end
+
+  def visa_debit_card() do
+    %{
+      card: [
+        number: "4000056655665556",
+        exp_month: exp_month(),
+        exp_year: exp_year(),
+        cvc: "314",
+        currency: "usd"
+      ]
+    }
+  end
+
+  def master_card_debit_card() do
+    %{
+      card: [
+        number: "5200828282828210",
+        exp_month: exp_month(),
+        exp_year: exp_year(),
+        cvc: "314",
+        currency: "usd"
+      ]
+    }
   end
 
   defp exp_month() do
@@ -23,6 +50,6 @@ defmodule Stripe.Fixture.Token do
   end
 
   defp exp_year() do
-    DateTime.utc_now.year + 1
+    DateTime.utc_now().year + 1
   end
 end
